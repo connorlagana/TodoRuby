3795d36dc733adad19c3412efec4f55d2570a01a
Connor Lagana <connor.lagana@gmail.com>
2020-02-07 16:13:46 -0500

    update form

--- todo_rest/client/src/components/UpdateTodoForm.js
+++ todo_rest/client/src/components/UpdateTodoForm.js
@@ -1,2 +1,17 @@
 import React, { Component } from "react";
-export default class RegisterForm extends Component {}
+export default class RegisterForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: ""
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>helllo</h1>
+      </div>
+    );
+  }
+}
